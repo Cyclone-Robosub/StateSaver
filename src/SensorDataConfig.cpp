6622d6d44e8d4155518e50efcb9cfd858cade92f
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2025-07-12 11:42:52 -0700

    Kory's Data update with the list provided Gyro Accel Mag Plus Roll, pitch, yaw (may be called heading) from the AHRS

--- src/SensorDataConfig.cpp
+++ src/SensorDataConfig.cpp
@@ -1,123 +1,174 @@
 #include "SensorDataConfig.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include <iomanip>
 
-SensorsDataConfig::SensorsDataConfig(std::ofstream& outputStateFile)
-          : Node("sensorsNode"), m_stateFile(outputStateFile) {
-    callbackDepthPressure = this->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive);
-    callbackIMU = this->create_callback_group(
-        rclcpp::CallbackGroupType::MutuallyExclusive);
-    callbackClTool =
-        this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-    callbackManual =
-        this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
 
-    auto commandOptions = rclcpp::SubscriptionOptions();
-    commandOptions.callback_group = callbackClTool;
-    auto durationOptions = rclcpp::SubscriptionOptions();
-    durationOptions.callback_group = callbackClTool;
-    auto depthPressureOptions = rclcpp::SubscriptionOptions();
-    depthPressureOptions.callback_group = callbackDepthPressure;
-    auto imuOptions = rclcpp::SubscriptionOptions();
-    imuOptions.callback_group = callbackIMU;
-    std::cout << "Creating sensors subscriptions\n";
-    auto magOptions = rclcpp::SubscriptionOptions();
-    magOptions.callback_group = callbackIMU;
-    auto ManualToggleOptions = rclcpp::SubscriptionOptions();
-    ManualToggleOptions.callback_group = callbackManual;
-    auto ManualOverrideOptions = rclcpp::SubscriptionOptions();
-    ManualOverrideOptions.callback_group = callbackManual;
 
-    depth_pressure_sensor_subscription_ =
-        this->create_subscription<std_msgs::msg::String>(
-            "depthPressureSensorData", rclcpp::QoS(5),
-            std::bind(&SensorsDataConfig::depthPressureSensorCallback, this, std::placeholders::_1),
-            depthPressureOptions);
+SensorsDataConfig::SensorsDataConfig(std::ofstream &outputStateFile)
+    : Node("sensorsNode"), m_stateFile(outputStateFile) {
+  callbackDepthPressure =
+      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  callbackIMU =
+      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  callbackClTool =
+      this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+  callbackManual =
+      this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
 
-    // Priority
-    // Need to input IMU initialization with ROS.
+  auto commandOptions = rclcpp::SubscriptionOptions();
+  commandOptions.callback_group = callbackClTool;
+  auto durationOptions = rclcpp::SubscriptionOptions();
+  durationOptions.callback_group = callbackClTool;
+  auto depthPressureOptions = rclcpp::SubscriptionOptions();
+  depthPressureOptions.callback_group = callbackDepthPressure;
+  auto imuOptions = rclcpp::SubscriptionOptions();
+  imuOptions.callback_group = callbackIMU;
+  std::cout << "Creating sensors subscriptions\n";
+  auto magOptions = rclcpp::SubscriptionOptions();
+  magOptions.callback_group = callbackIMU;
+  // Fix: changed from rclcpp : SubscriptionOptions() to
+  // rclcpp::SubscriptionOptions()
+  auto odom_ins_ned_Options = rclcpp::SubscriptionOptions();
+  odom_ins_ned_Options.callback_group = callbackIMU;
+  auto ManualToggleOptions = rclcpp::SubscriptionOptions();
+  ManualToggleOptions.callback_group = callbackManual;
+  auto ManualOverrideOptions = rclcpp::SubscriptionOptions();
+  ManualOverrideOptions.callback_group = callbackManual;
 
-    imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
-        "imu", rclcpp::QoS(5),
-        std::bind(&SensorsDataConfig::imuSensorCallback, this, std::placeholders::_1),
-        imuOptions);
-    mag_subscription_ =
-        this->create_subscription<sensor_msgs::msg::MagneticField>(
-            "mag", rclcpp::QoS(5),
-            std::bind(&SensorsDataConfig::magCallback, this, std::placeholders::_1),
-            magOptions);
-
-                //Please implement Kory's data as soon as possible
-    /*did_ins_subscription =
-    this->create_subscription<sensor_msgs::msg::MagneticField>(
-        "mag", rclcpp::QoS(5),
-        std::bind(&ExecutiveLoop::, mainLoopObject,
+  depth_pressure_sensor_subscription_ =
+      this->create_subscription<std_msgs::msg::String>(
+          "depthPressureSensorData", rclcpp::QoS(5),
+          std::bind(&SensorsDataConfig::depthPressureSensorCallback, this,
                     std::placeholders::_1),
-        did_ins_Options);*/
-    CLTool_subscription_ =
-        this->create_subscription<std_msgs::msg::Int32MultiArray>(
-            "array_Cltool_topic", rclcpp::QoS(10),
-            std::bind(&SensorsDataConfig::PWMArrayCallback, this, std::placeholders::_1),
-            commandOptions);
-    duration_subscription_ = this->create_subscription<std_msgs::msg::Int64>(
-        "duration_Cltool_topic", rclcpp::QoS(10),
-        std::bind(&SensorsDataConfig::durationCallback, this, std::placeholders::_1),
-        durationOptions);
-    Manual_Control_sub = this->create_subscription<std_msgs::msg::Bool>(
-        "manual_toggle_switch", rclcpp::QoS(10),
-        std::bind(&SensorsDataConfig::ManualControlCallback, this, std::placeholders::_1),
-        ManualToggleOptions);
-    Manual_Override_sub = this->create_subscription<std_msgs::msg::Empty>(
-        "manualOverride", rclcpp::QoS(4),
-        std::bind(&SensorsDataConfig::ManualOverrideCallback, this, std::placeholders::_1),
-        ManualOverrideOptions);
+          depthPressureOptions);
 
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&SensorsDataConfig::writeDataToFile, this));
+  // Priority
+  // Need to input IMU initialization with ROS.
+
+  imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+      "imu", rclcpp::QoS(5),
+      std::bind(&SensorsDataConfig::imuSensorCallback, this,
+                std::placeholders::_1),
+      imuOptions);
+  mag_subscription_ =
+      this->create_subscription<sensor_msgs::msg::MagneticField>(
+          "mag", rclcpp::QoS(5),
+          std::bind(&SensorsDataConfig::magCallback, this,
+                    std::placeholders::_1),
+          magOptions);
+
+  // Please implement Kory's data as soon as possible
+  odom_ins_ned_subscription =
+      this->create_subscription<nav_msgs::msg::Odometry>(
+          "roll_pitch_yaw", rclcpp::QoS(5),
+          // Fix: Ensure callback function name matches
+          std::bind(&SensorsDataConfig::Odom_insCallback, this,
+                    std::placeholders::_1),
+          odom_ins_ned_Options);
+  CLTool_subscription_ =
+      this->create_subscription<std_msgs::msg::Int32MultiArray>(
+          "array_Cltool_topic", rclcpp::QoS(10),
+          std::bind(&SensorsDataConfig::PWMArrayCallback, this,
+                    std::placeholders::_1),
+          commandOptions);
+  duration_subscription_ = this->create_subscription<std_msgs::msg::Int64>(
+      "duration_Cltool_topic", rclcpp::QoS(10),
+      std::bind(&SensorsDataConfig::durationCallback, this,
+                std::placeholders::_1),
+      durationOptions);
+  Manual_Control_sub = this->create_subscription<std_msgs::msg::Bool>(
+      "manual_toggle_switch", rclcpp::QoS(10),
+      std::bind(&SensorsDataConfig::ManualControlCallback, this,
+                std::placeholders::_1),
+      ManualToggleOptions);
+  Manual_Override_sub = this->create_subscription<std_msgs::msg::Empty>(
+      "manualOverride", rclcpp::QoS(4),
+      std::bind(&SensorsDataConfig::ManualOverrideCallback, this,
+                std::placeholders::_1),
+      ManualOverrideOptions);
+
+  timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(100),
+      std::bind(&SensorsDataConfig::writeDataToFile, this));
 }
 
-void SensorsDataConfig::depthPressureSensorCallback(const std_msgs::msg::String::SharedPtr msg) {
-    depthPressureData_ = msg->data;
+void SensorsDataConfig::depthPressureSensorCallback(
+    const std_msgs::msg::String::SharedPtr msg) {
+  depthPressureData_ = msg->data;
 }
-void SensorsDataConfig::imuSensorCallback(const sensor_msgs::msg::Imu::SharedPtr msg) {
-    imuData_ = *msg;
+void SensorsDataConfig::imuSensorCallback(
+    const sensor_msgs::msg::Imu::SharedPtr msg) {
+  imuData_ = *msg;
 }
-void SensorsDataConfig::magCallback(const sensor_msgs::msg::MagneticField::SharedPtr msg) {
-    magData_ = *msg;
+void SensorsDataConfig::magCallback(
+    const sensor_msgs::msg::MagneticField::SharedPtr msg) {
+  magData_ = *msg;
 }
-void SensorsDataConfig::PWMArrayCallback(const std_msgs::msg::Int32MultiArray::SharedPtr msg) {
-    pwmData_ = *msg;
+void SensorsDataConfig::Odom_insCallback( 
+    const nav_msgs::msg::Odometry::SharedPtr msg) {
+  geometry_msgs::msg::Quaternion orientation_quat_msg =
+      msg->pose.pose.orientation;
+  tf2::Quaternion q;
+  tf2::fromMsg(orientation_quat_msg, q);
+  tf2::Matrix3x3 m(q);
+  m.getRPY(roll_r, pitch_r, yaw_r);
 }
-void SensorsDataConfig::durationCallback(const std_msgs::msg::Int64::SharedPtr msg) {
-    durationData_ = msg->data;
+void SensorsDataConfig::PWMArrayCallback(
+    const std_msgs::msg::Int32MultiArray::SharedPtr msg) {
+  pwmData_ = *msg;
 }
-void SensorsDataConfig::ManualControlCallback(const std_msgs::msg::Bool::SharedPtr msg) {
-    manualControlData_ = msg->data;
+void SensorsDataConfig::durationCallback(
+    const std_msgs::msg::Int64::SharedPtr msg) {
+  durationData_ = msg->data;
 }
-void SensorsDataConfig::ManualOverrideCallback(const std_msgs::msg::Empty::SharedPtr msg) {
-    manualOverrideTriggered_ = true;
+void SensorsDataConfig::ManualControlCallback(
+    const std_msgs::msg::Bool::SharedPtr msg) {
+  manualControlData_ = msg->data;
+}
+void SensorsDataConfig::ManualOverrideCallback(
+    const std_msgs::msg::Empty::SharedPtr msg) {
+  manualOverrideTriggered_ = true;
 }
 
 void SensorsDataConfig::writeDataToFile() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-    m_stateFile << std::put_time(std::localtime(&currentTime), "%F %T") << ","
-                     << "Manual Override Triggered" << std::endl;
-    m_stateFile << std::put_time(std::localtime(&currentTime), "%F %T") << ",";
-    m_stateFile << depthPressureData_ << ",";
+  auto now = std::chrono::system_clock::now();
+  std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+  m_stateFile << std::put_time(std::localtime(&currentTime), "%F %T") << ","
+              << "Manual Override Triggered" << std::endl;
+  m_stateFile << std::put_time(std::localtime(&currentTime), "%F %T") << ",";
+  m_stateFile << depthPressureData_ << ",";
 
-    m_stateFile << imuData_.orientation.x << "," << imuData_.orientation.y << ","
-                     << imuData_.orientation.z << "," << imuData_.orientation.w << ",";
+  m_stateFile << imuData_.orientation.x << "," << imuData_.orientation.y << ","
+              << imuData_.orientation.z << "," << imuData_.orientation.w << ",";
 
-    for (size_t i = 0; i < pwmData_.data.size(); ++i) {
-        m_stateFile << pwmData_.data[i] << (i == pwmData_.data.size() - 1 ? "" : ",");
-    }
-    m_stateFile << ",";
-    m_stateFile << durationData_ << ",";
-    m_stateFile << manualControlData_ << ",";
-    m_stateFile << (manualOverrideTriggered_ ? "true" : "false") << std::endl;
+  // ADDED: Gyro (Angular Velocity)
+  m_stateFile << imuData_.angular_velocity.x << ","
+              << imuData_.angular_velocity.y << ","
+              << imuData_.angular_velocity.z << ",";
 
-    manualOverrideTriggered_ = false;
-	
+  // ADDED: Accel (Linear Acceleration)
+  m_stateFile << imuData_.linear_acceleration.x << ","
+              << imuData_.linear_acceleration.y << ","
+              << imuData_.linear_acceleration.z << ",";
+
+  // ADDED: Mag (Magnetic Field)
+  m_stateFile << magData_.magnetic_field.x << "," << magData_.magnetic_field.y
+              << "," << magData_.magnetic_field.z << ",";
+
+  // ADDED: Roll, Pitch, Yaw
+  m_stateFile << roll_r << "," << pitch_r << "," << yaw_r << ",";
+
+  for (size_t i = 0; i < pwmData_.data.size(); ++i) {
+    m_stateFile << pwmData_.data[i]
+                << (i == pwmData_.data.size() - 1 ? "" : ",");
+  }
+  m_stateFile << ",";
+  m_stateFile << durationData_ << ",";
+  m_stateFile << manualControlData_ << ",";
+  m_stateFile << (manualOverrideTriggered_ ? "true" : "false") << std::endl;
+
+  manualOverrideTriggered_ = false;
 }
-
-
--- src/SensorDataConfig.hpp
+++ src/SensorDataConfig.hpp
@@ -1,10 +1,21 @@
 #ifndef SENSORDATACONFIG_HPP
 #define SENSORDATACONFIG_HPP
+
 #include "rclcpp/rclcpp.hpp"
 #include <chrono>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
+
+// Include for nav_msgs/msg/Odometry
+#include <nav_msgs/msg/odometry.hpp>
+
+// Includes for tf2 and geometry_msgs conversions
+#include "tf2/LinearMath/Matrix3x3.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp" // Required for tf2::fromMsg
+
+// Standard ROS message types
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/magnetic_field.hpp>
 #include <std_msgs/msg/bool.hpp>
@@ -15,42 +26,63 @@
 
 class SensorsDataConfig : public rclcpp::Node {
 public:
+  // Constructor
   SensorsDataConfig(std::ofstream &stateFile);
-void writeDataToFile();
+
+  // Public method to write data to file
+  void writeDataToFile();
 
 private:
-rclcpp::CallbackGroup::SharedPtr callbackDepthPressure;
-rclcpp::CallbackGroup::SharedPtr callbackIMU;
-rclcpp::CallbackGroup::SharedPtr callbackClTool;
-rclcpp::CallbackGroup::SharedPtr callbackManual;
-rclcpp::Subscription<std_msgs::msg::String>::SharedPtr
-    depth_pressure_sensor_subscription_;
-rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
-rclcpp::Subscription<sensor_msgs::msg::MagneticField>::SharedPtr
-    mag_subscription_;
-rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr
-    CLTool_subscription_;
-rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr duration_subscription_;
-rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr Manual_Control_sub;
-rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr Manual_Override_sub;
-std::ofstream &m_stateFile;
+  // Callback groups for managing execution
+  rclcpp::CallbackGroup::SharedPtr callbackDepthPressure;
+  rclcpp::CallbackGroup::SharedPtr callbackIMU;
+  rclcpp::CallbackGroup::SharedPtr callbackClTool;
+  rclcpp::CallbackGroup::SharedPtr callbackManual;
 
-std::string depthPressureData_;
-sensor_msgs::msg::Imu imuData_;
-sensor_msgs::msg::MagneticField magData_;
-std_msgs::msg::Int32MultiArray pwmData_;
-int64_t durationData_;
-bool manualControlData_;
-bool manualOverrideTriggered_ = false;
-rclcpp::TimerBase::SharedPtr timer_;
+  // Subscriptions
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr
+      depth_pressure_sensor_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::MagneticField>::SharedPtr
+      mag_subscription_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr
+      odom_ins_ned_subscription;
+  rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr
+      CLTool_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr duration_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr Manual_Control_sub;
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr Manual_Override_sub;
 
-void depthPressureSensorCallback(const std_msgs::msg::String::SharedPtr msg);
-void imuSensorCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
-void magCallback(const sensor_msgs::msg::MagneticField::SharedPtr msg);
-void PWMArrayCallback(const std_msgs::msg::Int32MultiArray::SharedPtr msg);
-void durationCallback(const std_msgs::msg::Int64::SharedPtr msg);
-void ManualControlCallback(const std_msgs::msg::Bool::SharedPtr msg);
-void ManualOverrideCallback(const std_msgs::msg::Empty::SharedPtr msg);
-}
-;
+  // Output file stream reference
+  std::ofstream &m_stateFile;
+
+  // Data storage variables for sensor messages
+  std::string depthPressureData_;
+  sensor_msgs::msg::Imu imuData_;
+  sensor_msgs::msg::MagneticField magData_;
+  std_msgs::msg::Int32MultiArray pwmData_;
+
+  // Variables to store Roll, Pitch, Yaw from Odometry callback
+  double roll_r, pitch_r, yaw_r;
+
+  // Other data variables
+  int64_t durationData_;
+  bool manualControlData_;
+  bool manualOverrideTriggered_ = false; // Initialized to false
+
+  // Timer for periodic data writing
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  // Callback functions for subscriptions
+  void depthPressureSensorCallback(const std_msgs::msg::String::SharedPtr msg);
+  void imuSensorCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
+  void magCallback(const sensor_msgs::msg::MagneticField::SharedPtr msg);
+  void PWMArrayCallback(const std_msgs::msg::Int32MultiArray::SharedPtr msg);
+  void durationCallback(const std_msgs::msg::Int64::SharedPtr msg);
+  void ManualControlCallback(const std_msgs::msg::Bool::SharedPtr msg);
+  void ManualOverrideCallback(const std_msgs::msg::Empty::SharedPtr msg);
+  // Callback for nav_msgs::msg::Odometry
+  void Odom_insCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+};
+
 #endif // SENSORDATACONFIG_HPP
