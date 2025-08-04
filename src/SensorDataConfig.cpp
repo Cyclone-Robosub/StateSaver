234fe37a117059811d38bccd91ef593961ca82c6
flametd14 <107810692+StrongStaples786@users.noreply.github.com>
2025-08-04 13:40:52 -0700

    random num to prove no need for mutex or atomic i believe.

--- src/SensorDataConfig.cpp
+++ src/SensorDataConfig.cpp
@@ -1,133 +1,153 @@
-#include "SensorDataConfig.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-#include "tf2/LinearMath/Matrix3x3.h"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include <iomanip>
+#include <gtest/gtest.h>
+#include <rclcpp/rclcpp.hpp>
+#include <filesystem>
+#include <fstream>
+#include <thread>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <iostream>
 
-SensorsDataConfig::SensorsDataConfig(std::ofstream &outputStateFile)
-    : Node("sensorsNode"), m_stateFile(outputStateFile) {
-  callbackDepthPressure =
-      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-  callbackIMU =
-      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-  callbackClTool =
-      this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-  callbackManual =
-      this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+#include "../src/SensorDataConfig.hpp"
+namespace fs = std::filesystem;
+std::ofstream makeStateFile() {
+  std::ofstream stateFile;
+  fs::path stateFilePath = fs::current_path().parent_path().parent_path();
+  std::string stateFileString =
+      stateFilePath / "state.csv"; // Use / for path concatenation
 
-  auto commandOptions = rclcpp::SubscriptionOptions();
-  commandOptions.callback_group = callbackClTool;
-  auto durationOptions = rclcpp::SubscriptionOptions();
-  durationOptions.callback_group = callbackClTool;
-  auto depthPressureOptions = rclcpp::SubscriptionOptions();
-  depthPressureOptions.callback_group = callbackDepthPressure;
-  auto imuOptions = rclcpp::SubscriptionOptions();
-  imuOptions.callback_group = callbackIMU;
-  std::cout << "Creating sensors subscriptions\n";
-  auto magOptions = rclcpp::SubscriptionOptions();
-  magOptions.callback_group = callbackIMU;
-  // Fix: changed from rclcpp : SubscriptionOptions() to
-  // rclcpp::SubscriptionOptions()
-  auto odom_ins_ned_Options = rclcpp::SubscriptionOptions();
-  odom_ins_ned_Options.callback_group = callbackIMU;
-  auto ManualToggleOptions = rclcpp::SubscriptionOptions();
-  ManualToggleOptions.callback_group = callbackManual;
-  auto ManualOverrideOptions = rclcpp::SubscriptionOptions();
-  ManualOverrideOptions.callback_group = callbackManual;
+  // Improved file handling: Create a new file each time.  You could
+  // alternatively implement a more sophisticated rotation strategy
+  stateFile.open(
+      stateFileString,
+      std::ofstream::app); // Open for output, truncating if it exists
 
-  depth_pressure_sensor_subscription_ =
-      this->create_subscription<std_msgs::msg::String>(
-          "depthPressureSensorData", rclcpp::QoS(5),
-          std::bind(&SensorsDataConfig::depthPressureSensorCallback, this,
-                    std::placeholders::_1),
-          depthPressureOptions);
-
-  // Priority
-  // Need to input IMU initialization with ROS.
-
-  imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
-      "imu", rclcpp::QoS(5),
-      std::bind(&SensorsDataConfig::imuSensorCallback, this,
-                std::placeholders::_1),
-      imuOptions);
-  mag_subscription_ =
-      this->create_subscription<sensor_msgs::msg::MagneticField>(
-          "mag", rclcpp::QoS(5),
-          std::bind(&SensorsDataConfig::magCallback, this,
-                    std::placeholders::_1),
-          magOptions);
-
-  // Please implement Kory's data as soon as possible
-  odom_ins_ned_subscription =
-      this->create_subscription<nav_msgs::msg::Odometry>(
-          "roll_pitch_yaw", rclcpp::QoS(5),
-          // Fix: Ensure callback function name matches
-          std::bind(&SensorsDataConfig::Odom_insCallback, this,
-                    std::placeholders::_1),
-          odom_ins_ned_Options);
-
-  timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(100),
-      std::bind(&SensorsDataConfig::writeDataToFile, this));
-}
-
-void SensorsDataConfig::depthPressureSensorCallback(
-    const std_msgs::msg::String::SharedPtr msg) {
-  depthPressureData_ = msg->data;
-}
-void SensorsDataConfig::imuSensorCallback(
-    const sensor_msgs::msg::Imu::SharedPtr msg) {
-  imuData_ = *msg;
-}
-void SensorsDataConfig::magCallback(
-    const sensor_msgs::msg::MagneticField::SharedPtr msg) {
-  magData_ = *msg;
-}
-void SensorsDataConfig::Odom_insCallback(
-    const nav_msgs::msg::Odometry::SharedPtr msg) {
-  geometry_msgs::msg::Quaternion orientation_quat_msg =
-      msg->pose.pose.orientation;
-  tf2::Quaternion q;
-  tf2::fromMsg(orientation_quat_msg, q);
-  tf2::Matrix3x3 m(q);
-  m.getRPY(roll_r, pitch_r, yaw_r);
-}
-
-void SensorsDataConfig::writeDataToFile() {
-  auto now = std::chrono::system_clock::now();
-  std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-  m_stateFile << std::put_time(std::localtime(&currentTime), "%F %T") << ",";
-  m_stateFile << depthPressureData_ << ",";
-
-  m_stateFile << imuData_.orientation.x << "," << imuData_.orientation.y << ","
-              << imuData_.orientation.z << "," << imuData_.orientation.w << ",";
-
-  // ADDED: Gyro (Angular Velocity)
-  m_stateFile << imuData_.angular_velocity.x << ","
-              << imuData_.angular_velocity.y << ","
-              << imuData_.angular_velocity.z << ",";
-
-  // ADDED: Accel (Linear Acceleration)
-  m_stateFile << imuData_.linear_acceleration.x << ","
-              << imuData_.linear_acceleration.y << ","
-              << imuData_.linear_acceleration.z << ",";
-
-  // ADDED: Mag (Magnetic Field)
-  m_stateFile << magData_.magnetic_field.x << "," << magData_.magnetic_field.y
-              << "," << magData_.magnetic_field.z << ",";
-
-  // ADDED: Roll, Pitch, Yaw
-  m_stateFile << roll_r << "," << pitch_r << "," << yaw_r << ",";
-
-  for (size_t i = 0; i < pwmData_.data.size(); ++i) {
-    m_stateFile << pwmData_.data[i]
-                << (i == pwmData_.data.size() - 1 ? "" : ",");
+  if (stateFile.is_open()) {
+    stateFile << "Time,Depth(m),Pressure,IMU "
+                 "Data,Gyro,Accel,Mag,Roll,Pitch,Yaw,PWM Data"
+              << std::endl;
+    std::cout << "Created/Overwrote state file: " << stateFileString
+              << std::endl;
+  } else {
+    std::cerr << "Error opening state file: " << stateFileString << std::endl;
   }
-  m_stateFile << std::endl;
-  if (m_stateFile.tellp() > 100) {
-    m_stateFile.flush();
-    m_stateFile.clear();
-    m_stateFile.seekp(0);
-  }
+  return stateFile;
 }
+std::ofstream THISISTHEONE = makeStateFile();
+class SensorsDataConfigTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        rclcpp::init(0, nullptr);
+        std::filesystem::remove_all("state");
+        
+        temp_node_ = rclcpp::Node::make_shared("test_node");
+        imu_pub_ = temp_node_->create_publisher<sensor_msgs::msg::Imu>("imu", 10);
+        mag_pub_ = temp_node_->create_publisher<sensor_msgs::msg::MagneticField>("mag", 10);
+        odo_pub_ = temp_node_->create_publisher<nav_msgs::msg::Odometry>("roll_pitch_yaw", 10);
+        depth_pub_ = temp_node_->create_publisher<std_msgs::msg::String>("depthPressureSensorData", 10);
+    }
+
+    void TearDown() override {
+        rclcpp::shutdown();
+    }
+
+    rclcpp::Node::SharedPtr temp_node_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr mag_pub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odo_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr depth_pub_;
+};
+
+// Test CSV file initialization
+TEST_F(SensorsDataConfigTest, TestCSVInitialization) {
+    auto node = std::make_shared<SensorsDataConfig>(THISISTHEONE);
+  std::cout << "ROS2 StateSaver start running" << std::endl;
+  std::jthread ROSthread([node](){rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);executor.spin();});
+  std::cout << "here" <<std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    fs::path stateFilePath = fs::current_path().parent_path().parent_path() / "state.csv";
+    EXPECT_TRUE(fs::exists(stateFilePath));
+    
+    // Check the header content
+    std::ifstream file(stateFilePath);
+    ASSERT_TRUE(file.is_open());
+    std::string header;
+    std::getline(file, header);
+    
+    // Check if header contains expected columns
+    EXPECT_EQ(header, "Time,Depth(m),Pressure,IMU Data,Gyro,Accel,Mag,Roll,Pitch,Yaw,PWM Data");
+    rclcpp::shutdown();
+}
+
+
+// Test depth pressure callback
+TEST_F(SensorsDataConfigTest, TestDepthPressureCallback) {
+    auto node = std::make_shared<SensorsDataConfig>(THISISTHEONE);
+    std::cout << "ROS2 StateSaver start running.  Please wait for 3 seconds" << std::endl;
+  std::jthread ROSthread([node](){rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);executor.spin();});
+    auto depth_msg = std::make_shared<std_msgs::msg::String>();
+    depth_msg->data = "123.45";
+    
+    depth_pub_->publish(*depth_msg);
+    
+    
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    
+    EXPECT_TRUE(true); // Callback processed without crashing
+   rclcpp::shutdown();
+}
+
+// Test IMU callback
+TEST_F(SensorsDataConfigTest, TestIMUCallback) {
+    auto node = std::make_shared<SensorsDataConfig>(THISISTHEONE);
+     std::cout << "ROS2 StateSaver start running. Please wait for 3 seconds" << std::endl;
+  std::jthread ROSthread([node](){rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);executor.spin();});
+    auto imu_msg = std::make_shared<sensor_msgs::msg::Imu>();
+    imu_msg->angular_velocity.x = 1.0;
+    imu_msg->angular_velocity.y = 2.0;
+    imu_msg->angular_velocity.z = 3.0;
+    imu_msg->linear_acceleration.x = 0.1;
+    imu_msg->linear_acceleration.y = 0.2;
+    imu_msg->linear_acceleration.z = 9.8;
+    
+    imu_pub_->publish(*imu_msg);
+    
+    
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    
+    EXPECT_TRUE(true); // Callback processed without crashing // For now use your eyes.
+    rclcpp::shutdown(); //Remove this to see how long it goes.
+}
+TEST_F(SensorsDataConfigTest, IntentionalWaiting){
+    std::cout << "THIS TEST IS MEANT TO BE STUCK/APPEAR FROZEN. After a while, please hit Control-C two times." << std::endl;
+    std::jthread PublisherThread([this](){
+      int min = 1;
+    int max = 100;
+         auto imu_msg = std::make_shared<sensor_msgs::msg::Imu>();
+        while(true){
+        imu_msg->angular_velocity.x = 1.0;
+        imu_msg->angular_velocity.y = min + (rand() % (max - min + 1));
+        imu_msg->angular_velocity.z = 2525.0;
+        imu_msg->linear_acceleration.x = 0.1;
+        imu_msg->linear_acceleration.y = 325352535;
+        imu_msg->linear_acceleration.z = 342;
+        this->imu_pub_->publish(*imu_msg);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+    });
+    auto sensorsNode = std::make_shared<SensorsDataConfig>(THISISTHEONE);
+    rclcpp::executors::MultiThreadedExecutor executor;
+     executor.add_node(sensorsNode);
+    std::cout << "ROS2 StateSaver running" << std::endl;
+    executor.spin();
+   rclcpp::shutdown();
+  std::cout << "Shutting Down" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+} 
